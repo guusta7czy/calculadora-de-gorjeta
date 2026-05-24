e8c743e716b00db3997bd2fdda6321484ac3562b
Gustavo Mercês <gustavomercesdonascimento@gmail.com>
2026-05-24 19:41:25 -0300

    nono commit css e terceiro commit css

--- script.js
+++ script.js
@@ -28,9 +28,17 @@ function receberQuantidadePessoas() {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type'button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao)
 })
 
-function receberPorcentagem(evento) {
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
 
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) /100
 }
